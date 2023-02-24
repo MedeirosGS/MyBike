d6a54c9b45e24716b9561d4abc0485132c99a37a
MedeirosGS <gabriel.medeiros57@etec.sp.gov.br>
2023-02-23 21:37:38 -0300

    segunda pagina

---
+++ app/src/main/java/br/etecia/mybike/Splash_Activiy.java
@@ -0,0 +1,13 @@
+package br.etecia.mybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Activiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activiy);
+    }
+}
